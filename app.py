3d15390029c951e30b2039edab88e4e742ad369c
pratheekms <ms.pratheek1@gmail.com>
2021-11-10 21:59:49 +0530

    added json return to app

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import flask
-app=flask(__name__)
+from flask import Flask,jsonify
+app=Flask(__name__)
 
-@app.route(/)
+@app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/name')
+def hello_name():
+    return 'hello pratheek'
+
+@app.route('/details')
+def details_json():
+    retJson={
+    'name':'pratheek',
+    'age':25,
+    'contact':[
+    {
+    'email':'p@gmail.com',
+    'phone':'9393939'
+    },
+    {
+    'phone2':242353252
+    }
+    ]
+    }
+
+    return jsonify(retJson)
+
+
+if __name__=="__main__":
+    app.run()
