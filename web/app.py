9921f76715c6136c8af27658b31aab1eb5195c6e
pratheekms <ms.pratheek1@gmail.com>
2021-11-13 16:58:32 +0530

    added docker required files

---
+++ web/app.py
@@ -0,0 +1,198 @@
+from flask import Flask, jsonify,request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api=Api(app)
+
+def checkPostedData(postedData, functionName):
+    if (functionName == "add" or functionName == "subtract" or functionName == "multiply"):
+        if "x" not in postedData or "y" not in postedData:
+            return 301 #Missing parameter
+        else:
+            return 200
+    elif (functionName == "division"):
+        if "x" not in postedData or "y" not in postedData:
+            return 301
+        elif int(postedData["y"])==0:
+            return 302
+        else:
+            return 200
+
+class Add(Resource):
+    def post(self):
+        #If I am here, then the resouce Add was requested using the method POST
+
+        #Step 1: Get posted data:
+        postedData = request.get_json()
+
+        #Steb 1b: Verify validity of posted data
+        status_code = checkPostedData(postedData, "add")
+        if (status_code!=200):
+            retJson = {
+                "Message": "An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+
+        #If i am here, then status_code == 200
+        x = postedData["x"]
+        y = postedData["y"]
+        x = int(x)
+        y = int(y)
+
+        #Step 2: Add the posted data
+        ret = x+y
+        retMap = {
+            'Message': ret,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
+
+class Subtract(Resource):
+    def post(self):
+        #If I am here, then the resouce Subtract was requested using the method POST
+
+        #Step 1: Get posted data:
+        postedData = request.get_json()
+
+        #Steb 1b: Verify validity of posted data
+        status_code = checkPostedData(postedData, "subtract")
+
+
+        if (status_code!=200):
+            retJson = {
+                "Message": "An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+
+        #If i am here, then status_code == 200
+        x = postedData["x"]
+        y = postedData["y"]
+        x = int(x)
+        y = int(y)
+
+        #Step 2: Subtract the posted data
+        ret = x-y
+        retMap = {
+            'Message': ret,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
+
+
+class Multiply(Resource):
+    def post(self):
+        #If I am here, then the resouce Multiply was requested using the method POST
+
+        #Step 1: Get posted data:
+        postedData = request.get_json()
+
+        #Steb 1b: Verify validity of posted data
+        status_code = checkPostedData(postedData, "multiply")
+
+
+        if (status_code!=200):
+            retJson = {
+                "Message": "An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+
+        #If i am here, then status_code == 200
+        x = postedData["x"]
+        y = postedData["y"]
+        x = int(x)
+        y = int(y)
+
+        #Step 2: Multiply the posted data
+        ret = x*y
+        retMap = {
+            'Message': ret,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
+
+class Divide(Resource):
+    def post(self):
+        #If I am here, then the resouce Divide was requested using the method POST
+
+        #Step 1: Get posted data:
+        postedData = request.get_json()
+
+        #Steb 1b: Verify validity of posted data
+        status_code = checkPostedData(postedData, "division")
+
+
+        if (status_code!=200):
+            retJson = {
+                "Message": "An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+
+        #If i am here, then status_code == 200
+        x = postedData["x"]
+        y = postedData["y"]
+        x = int(x)
+        y = int(y)
+
+        #Step 2: Multiply the posted data
+        ret = (x*1.0)/y
+        retMap = {
+            'Message': ret,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
+
+
+
+api.add_resource(Add, "/add")
+api.add_resource(Subtract, "/subtract")
+api.add_resource(Multiply, "/multiply")
+api.add_resource(Divide, "/division")
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/name')
+def hello_name():
+    return 'hello pratheek'
+
+
+@app.route('/details')
+def details_json():
+    retJson = {
+        'name': 'pratheek',
+        'age': 25,
+        'contact': [
+            {
+                'email': 'p@gmail.com',
+                'phone': '9393939'
+            },
+            {
+                'phone2': 242353252
+            }
+        ]
+    }
+
+    return jsonify(retJson)
+
+@app.route('/addition',methods=["POST"])
+def addition():
+    d_dict=request.get_json()
+    num1=d_dict["x"]
+    num2=d_dict["y"]
+    sum=num1+num2
+    retjson={
+        'Sum':sum
+    }
+    return jsonify(retjson),200
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
